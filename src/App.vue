6ab2e1f9f688e211c0ddcfd08539d2e0f0d378bb
Alex Mwangi <alex@stl-horizon.com>
2022-06-23 10:43:36 +0300

    Replacing data with refs

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,64 @@
+
+<!--  1) Replacing "data" with "setup"
+======================================
+
+-Done by adding the setup() {}
+
+-If we had components registered locally or props , that remains the same
+
+-We have 2 new features offered by Vue which helps us manage our data which we need to import from vue
+
+  1.) ref 
+        -is a function which we can call inside setup(){}
+
+        -creates a reference not to some DOM element ; but a value which we can use in our template
+
+        -creates a value but not just a value but a reactive value where Vue will find out when we change it , watch it and update the template 
+
+        -And since it creates a value, we need to store it in some constant as a variable
+
+        - We might wanna store a Number / String which we can therefore pass to ref()
+
+        -And this now creates a reactive string you could say
+
+        -Under the hood , it creates an obj and the string is stored in that object;
+
+        -Vue will watch this value and updates it everytime it changes
+
+        -However , const userName is still not available in the template
+
+        -We need to return an object - ALWAYS RETURN AN OBJECT - AND IN THIS OBJECT WE RETURN ONLY THE THINGS WE NEED TO EXPOSE TO THE TEMPLATE,
+
+        -We return a key : value pair ; WHERE key is the one used in the template and value is the value returned 
+
+          const uName = 'Maximillian'
+
+      -IF THE ABOVE STILL THIS WORKS , WHY DO WE NEED ref THEN?     -It's because it creates a reactive value 
+
+      EXAMPLE
+      ===========
+
+       setup(){
+
+          const uName = ref('Maximilian')
+          
+          setTimeout(()=>{
+
+            uName.value = 'Max';
+
+          },2000 )
+
+          return {
+              userName : uName
+          };
+    }
+
+    -Sets a timeout that updates name to Max after 2 seconds
+
+
+ -->
+
+
 <template>
   <section class="container">
     <h2>{{ userName }}</h2>
@@ -5,12 +66,31 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
-  data() {
+  
+  // data() {
+  //   return {
+  //     userName: 'Maximilian',
+  //   };
+  // },
+  setup(){
+
+    const uName = ref('Maximilian')
+
+    console.log(uName.value);
+
+    setTimeout(()=>{
+
+      uName.value = 'Max';
+
+    },2000 )
+
     return {
-      userName: 'Maximilian',
+        userName : uName
     };
-  },
+  }
 };
 </script>
 
